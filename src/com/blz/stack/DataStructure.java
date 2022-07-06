f82e245d1b712d42e41062b96bb59b39d63efcef
Him5696 <himanshi1252@gmail.com>
2022-07-06 15:51:27 +0530

    Creating Stack

--- src/com/blz/stack/DataStructure.java
+++ src/com/blz/stack/DataStructure.java
@@ -2,7 +2,8 @@ package com.blz.stack;
 
 public class DataStructure {
     public static void main(String[] args) {
-        System.out.println("Welcome to Stack And Queue Problem");
+        Stack newStack=new Stack();
+        newStack.push();
     }
 
 }
---
+++ src/com/blz/stack/Stack.java
@@ -0,0 +1,17 @@
+package com.blz.stack;
+
+public class Stack {
+    public Node top;
+    LinkedList linkedList;
+
+    public Stack() {
+        this.linkedList = new LinkedList();
+    }
+    public void push() {
+        linkedList.push(70);
+        linkedList.push(30);
+        top = linkedList.push(56);
+        linkedList.print();
+        System.out.println("Top -> " + top.data);
+    }
+}
